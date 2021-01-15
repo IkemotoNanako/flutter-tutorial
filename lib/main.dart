fee69a632f640a8ee303ea99572894506250279a
IkemotoNanako <chibencaizi@gmail.com>
2021-01-16 01:20:15 +0900

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
